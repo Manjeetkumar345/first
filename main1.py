db0419cfd86fc2900ea639b175cd1d7fc8f2afed
Manjeet0Kumar <manjeet.kumar_comp23@pccoer.in>
2024-09-05 00:11:29 +0530

    Initial commit with

--- main1.py
+++ main1.py
@@ -1,32 +1,34 @@
 print("Hello")
-<<<<<<< HEAD
+# <<<<<<< HEAD
 #<<<<<<< HEAD
 print("hi")
 print("Loda")
 
 print("Gihh")
 #=======
-=======
-<<<<<<< HEAD
->>>>>>> acc78749868b27e10fa7d3f023977ef778a2d588
+# =======
+# <<<<<<< HEAD
+# >>>>>>> acc78749868b27e10fa7d3f023977ef778a2d588
 print("hello world")
 
 print("anlle")
 print("nub")
-dscs
-sdgvs
-<<<<<<< HEAD
+# dscs
+# sdgvs
+# <<<<<<< HEAD
 #>>>>>>> c27972502181370e32bd0114fb31a138f0ebc314
 
 
 print("noob rexx")
-=======
-=======
+# =======
+# =======
 print("hi")
 print("Loda")
 
 print("Gihh")
 print("lassan")
 print("rupali bhabhi porn video")
->>>>>>> e214371ab2b802c2ad58dbb23fe3a04aabff1d5a
->>>>>>> acc78749868b27e10fa7d3f023977ef778a2d588
+# >>>>>>> e214371ab2b802c2ad58dbb23fe3a04aabff1d5a
+# >>>>>>> acc78749868b27e10fa7d3f023977ef778a2d588
+
+print("chuss")
