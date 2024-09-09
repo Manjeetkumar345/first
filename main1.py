e948423ea95e968f1aaf356b39b39ce27372f234
TanishkRexx <tanishkprajapati1010@gmail.com>
2024-09-09 20:22:14 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,8 +1,1 @@
-print("Hello")
-<<<<<<< HEAD
-print("hello world")
-=======
-print("hi")
->>>>>>> 68a0747c7df2a27fc0708a0c966ac966599f17ce
-
-print("anlle")
+https://github.com/TanishkRexx/firstgit.git
